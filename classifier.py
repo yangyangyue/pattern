c7a218850e244d7c5a2c8dfe5d47973ec6726f49
lily <3477408934@qq.com>
2023-01-17 23:47:45 +0800

    v0.0.0 First commit. There are some issue to solve.

---
+++ classifier.py
@@ -0,0 +1,171 @@
+import random
+from abc import ABCMeta, abstractmethod
+from collections import Counter
+
+import numpy as np
+import torch.nn as nn
+
+
+class CNNClassifier(nn.Module):
+    def __init__(self):
+        """
+        初始化CNN分类器
+        考虑到mnist数据集并不复杂，所以使用的CNN分类器也很简单，只包括一个简单的主干特征提取网络以及一个将输出映射为目标形状(10,)的全连接网络
+        """
+        super(CNNClassifier, self).__init__()
+        self.backbone = nn.Sequential(
+            self.__make_layer(1, 32),
+            self.__make_layer(32, 64)
+        )
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(64, 10)
+        )
+
+    @staticmethod
+    def __make_layer(in_channel, out_channel):
+        return nn.Sequential(
+            nn.Conv2d(in_channel, out_channel, (3, 3)),
+            nn.BatchNorm2d(out_channel),
+            nn.ReLU(),
+            nn.Conv2d(out_channel, out_channel, (3, 3)),
+            nn.BatchNorm2d(out_channel),
+            nn.MaxPool2d(2),
+        )
+
+    def forward(self, x):
+        x = self.backbone(x)
+        x = self.classifier(x)
+        return x
+
+
+class KNNClassifier:
+    def __init__(self, k: int):
+        self.k = k
+        self.n_class = 10
+
+    def __call__(self, train_data, train_targets, img):
+        # 计算输入与训练集中各样本的距离
+        dis = np.sum((train_data - img) ** 2, axis=(1, 2)) ** 0.5
+        # 统计前k个近邻中各个类别的数量
+        cls = np.zeros((self.n_class,))
+        for idx in np.argpartition(dis, self.k)[:self.k]:
+            cls[train_targets[idx]] += 1
+        return np.argmax(cls)
+
+
+class RandomForestClassifier:
+    # 大部分参数的设置其实没必要暴漏给用户，自己指定就行
+    def __init__(self, n_trees=10, max_depth=-1, min_samples_split=2, min_samples_leaf=1, sample_rate=0.3):
+        self.n_class = 10
+        self.n_trees = n_trees
+        self.trees = []
+        self.max_depth = max_depth if max_depth != -1 else float('inf')
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        self.sample_rate = sample_rate
+
+    def fit(self, samples, targets):
+        """
+        训练随机森林
+        :param samples: 训练集样本, shape:(m, 28, 28)
+        :param targets: 训练集标签, shape:(m,)
+        """
+        samples = samples.reshape((len(samples), -1))
+        print("随机森林模型训练中, 进度:", end=" 0.00")
+        for i in range(self.n_trees):
+            # 随机生成子数据集的索引，用于构造训练每颗树的数据集
+            idxs = random.sample(range(len(targets)), int(len(targets) * self.sample_rate))
+            self.trees.append(self.__build_tree(samples[idxs], targets[idxs], 0))
+            print(f"\b\b\b\b{(i+1) / self.n_trees:.2f}", end="")
+        print(f"\n随机森林训练完成, 决策树数：{self.n_trees}")
+
+    def __build_tree(self, samples, targets, depth):
+        """
+        构造一颗决策树，决策树是基于Gini指数的二叉树，使用递归的方法构造
+        :param samples: 样本，shape: (m, 28*28)
+        :param targets: 标签，shape: (m,)
+        :param depth: 当前深度， 用于限制决策树的深度
+        :return: 决策树 
+        """
+        # 样本全部是同一类别，则停止分裂
+        if all(target == targets[0] for target in targets):
+            return self.Tree(cls=targets[0])
+        # 样本数量小于分裂最低样本数或者深度达到限制，则停止分裂
+        if len(targets) < self.min_samples_split or depth >= self.max_depth:
+            return self.Tree(cls=Counter(targets).most_common(1)[0][0])
+        # 随机生成候选分裂特征集合
+        sub_features = random.sample(range(len(samples[0])), int(np.sqrt(len(samples[0]))))
+        # 选择分裂特征，虽然样本数很多，但是特征值的话大部分也都是重复的，因此无需基于百分位值计算分裂值
+        best_feature = 0
+        best_value = 0
+        best_gain = 1
+        for feature in sub_features:
+            # 排序, 去重
+            values = np.unique(samples[:, feature])
+            # 选择最佳分裂特征和分裂值
+            for value in values:
+                left_targets = targets[samples[:, feature] <= value]
+                right_targets = targets[samples[:, feature] > value]
+                if all(samples[:, feature] <= value) or all(samples[:, feature] > value):
+                    continue
+                gain = self.__gain(left_targets, right_targets)
+                if gain < best_gain:
+                    best_feature, best_value, best_gain = feature, value, gain
+
+        # 划分后所有样本都被划分到同一子集，则停止分裂
+        if all(samples[:, best_feature] <= best_value) or all(samples[:, best_feature] > best_value):
+            return self.Tree(cls=Counter(targets).most_common(1)[0][0])
+
+        tree = self.Tree(feature=best_feature, value=best_value)
+        tree.left = self.__build_tree(samples[samples[:, best_feature] <= best_value],
+                                      targets[samples[:, best_feature] <= best_value], depth + 1)
+        tree.right = self.__build_tree(samples[samples[:, best_feature] > best_value],
+                                       targets[samples[:, best_feature] > best_value], depth + 1)
+        return tree
+
+    def __call__(self, x):
+        """
+        对一个样本进行分类
+        """
+        x = x.reshape([-1])
+        cls = np.zeros((self.n_class,))
+        for c in (tree(x) for tree in self.trees):
+            cls[c] += 1
+        return np.argmax(cls)
+
+    @staticmethod
+    def __gini(targets):
+        num = len(targets)
+        # 数据集中每个类别的数量，用于计算随机样本属于各个类的概率
+        # cls_counts = np.zeros((self.n_class,))
+        # 本来是自己统计的， 但是太慢了， 使用counter花费时间大约是之前的1/3
+        gini = 1
+        for _, count in Counter(targets).most_common():
+            gini -= (count / num) ** 2
+        # for label in targets:
+        #     cls_counts[label] += 1
+        # for cls_count in cls_counts:
+        #     gini -= (cls_count / num) ** 2
+        return gini
+
+    def __gain(self, targets1, targets2):
+        return (len(targets1) * self.__gini(targets1) + len(targets2) * self.__gini(targets2)) / (
+                len(targets1) + len(targets2))
+
+    class Tree(object):
+
+        def __init__(self, feature=None, value=None, cls=None, left=None, right=None):
+            self.feature = feature  # 当前节点的分裂特征
+            self.value = value  # 当前节点的分裂特征值
+            self.cls = cls  # 当前节点的类别，如果当前节点不是叶子节点，则为None
+            self.left = left  # 左子树
+            self.right = right  # 右子树
+
+        def __call__(self, x):
+            # 叶子节点
+            if self.cls is not None:
+                return self.cls
+            # 小于分裂特征值则走左子树，大于分裂特征值则走右子树
+            return self.left(x) if x[self.feature] <= self.value else self.right(x)
---
+++ main.py
@@ -0,0 +1,167 @@
+import pickle
+
+import numpy as np
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+from classifier import KNNClassifier, CNNClassifier, RandomForestClassifier
+import click
+
+"""
+主函数，负责接收参数, 调用算法, 计算准确率
+ps: MNIST统一使用torch内置的数据集，避免没必要的逻辑
+"""
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    # 如果没有子命令的话，则按默认配置依次执行RF, KNN和CNN算法
+    if ctx.invoked_subcommand is None:
+        rf.main(standalone_mode=False)
+        knn.main(standalone_mode=False)
+        cnn.main(standalone_mode=False)
+
+
+@cli.command()
+@click.option('--k', default=10, help="KNN分类中的K值")
+@click.option('--count', default=100, help="KNN分类测试样本数量")
+def knn(k, count):
+    """k近邻分类"""
+    # 加载数据
+    train_data = datasets.MNIST(root="./data/", train=True, download=True)
+    test_data = datasets.MNIST(root="./data/", train=False, download=True)
+    assert 0 < k, "KNN近邻数量应大于0"
+    assert 0 < count <= len(test_data), f"KNN测试样本数量应在0到{len(test_data)}之间"
+    # 实例化分类器
+    knn_classifier = KNNClassifier(k)
+    # 对测试样本进行分类，并计算准确率指标
+    t = 0
+
+    print(f"KNN测试中, 进度:", end="     ", flush=True)
+    for idx in range(count):
+        y = knn_classifier(train_data.data.numpy().astype(np.float32), train_data.targets.numpy(),
+                           test_data.data[idx].numpy().astype(np.float32))
+        t += y == test_data.targets[idx].item()
+        print(f"\b\b\b\b{(idx + 1) / count:.2f}", end="", flush=True)
+    print(f"\rKNN测试完成, 测试样本数：{count}，测试准确率：{t / count:.4f}")
+
+
+@cli.command()
+@click.option('--his/--no-his', default=True, help="是否加载历史模型")
+@click.option('--checkpoint/--no-checkpoint', "check", default=False, help="是否加载检查点")
+@click.option('--lr', default=1e-4, help="CNN训练学习率")
+@click.option('--drop', default=30, help="CNN训练学习率衰减周期")
+@click.option("--epochs", default=100, help="CNN训练轮数")
+def cnn(his, check, lr, drop, epochs):
+    """卷积神经网络分类"""
+    # 如果有gpu则使用gpu， 否则使用cpu
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # 加载MNIST数据
+    train_data = datasets.MNIST(root="./data/", train=True, transform=transforms.ToTensor(), download=True)
+    test_data = datasets.MNIST(root="./data/", train=False, transform=transforms.ToTensor(), download=True)
+    train_loader = DataLoader(train_data, batch_size=64)
+    test_loader = DataLoader(test_data, batch_size=64)
+
+    # 模型、参数及优化器等
+    classifier = CNNClassifier().to(device)
+    params = [p for p in classifier.parameters() if p.requires_grad]
+    optimizer = torch.optim.Adam(params, lr=lr, betas=(0.9, 0.99))
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=drop, gamma=0.1)
+
+    if his:
+        # 加载已训练模型
+        checkpoint = torch.load('./his/model.pth')
+        classifier.load_state_dict(checkpoint['model_state'])
+        # 测试 & 计算准确率指标
+        print("CNN模型加载成功, 测试中, 进度:", end="     ", flush=True)
+        test_acc = 0
+        i = 0
+        for imgs, targets in test_loader:
+            imgs, targets = imgs.to(device), targets.to(device)
+            pred = classifier(imgs)
+            test_acc += (torch.argmax(pred, dim=1) == targets).sum()
+            i += 1
+            print(f"\b\b\b\b{i / len(test_loader):.2f}", end="", flush=True)
+        print(f"\rCNN测试完成, 测试样本数: {len(test_data)}, 测试准确率: {test_acc / len(test_data):.4f}")
+    else:
+        if check:
+            # 加载检查点
+            checkpoint = torch.load('./his/model.pth')
+            classifier.load_state_dict(checkpoint['model_state'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            start = checkpoint['epoch'] + 1
+            print("CNN检查点已加载")
+        else:
+            start = 0
+
+        # 按轮训练，测试
+        for epoch in range(start, epochs):
+            loss_fn = nn.CrossEntropyLoss()
+            train_loss = 0
+            train_acc = 0
+            test_loss = 0
+            test_acc = 0
+            print(f"CNN第{epoch}轮训练开始", end='...')
+            for imgs, targets in train_loader:
+                imgs, targets = imgs.to(device), targets.to(device)
+                pred = classifier(imgs)
+                loss = loss_fn(pred, targets)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                # 指标
+                train_loss += loss
+                train_acc += (torch.argmax(pred, dim=1) == targets).sum()
+            # 更新学习率
+            lr_scheduler.step()
+            print(f"\b\b\b\b\b结束, 训练误差： {train_loss / len(train_loader)}; 训练准确率：{train_acc / len(train_data)}")
+            print(f"CNN第{epoch}轮测试开始", end='...')
+            with torch.no_grad():
+                for imgs, targets in test_loader:
+                    pred = classifier(imgs)
+                    test_loss += loss_fn(pred, targets)
+                    test_acc += (torch.argmax(pred, dim=1) == targets).sum()
+            print(f"\b\b\b\b\b结束, 测试误差：{test_loss / len(test_loader)}; 测试准确率：{test_acc / len(test_data)}")
+
+            # 每轮保存一次模型、优化器及当前轮数，任务比较简单，不需要特别地保存训练效果最佳的模型
+            checkpoint = {'model_state': classifier.state_dict(), 'epoch': epoch, 'optimizer': optimizer.state_dict(), }
+            torch.save(checkpoint, './his/model.pth')
+
+
+@cli.command()
+@click.option('--his/--no-his', default=True, help="是否加载已训练模型")
+@click.option('--tree', "n_trees", default=10, help="随机森林中决策树数量")
+def rf(his, n_trees):
+    """
+    随机森林分类
+    """
+    # 加载数据
+    train_data = datasets.MNIST(root="./data/", train=True, download=True)
+    test_data = datasets.MNIST(root="./data/", train=False, download=True)
+
+    if his:
+        # 加载历史模型
+        print("RF模型加载成功, 测试中， 进度:", end="     ")
+        with open("./his/rf.pth", "rb") as f:
+            classifier = pickle.load(f)
+    else:
+        # 训练 & 保存模型
+        with open("./his/rf.pth", "wb") as f:
+            classifier = RandomForestClassifier(n_trees=n_trees)
+            classifier.fit(train_data.data.numpy(), train_data.targets.numpy())
+            pickle.dump(classifier, f)
+    # 测试
+    t = 0
+    i = 0
+    for sample, target in test_data:
+        t += target == classifier(np.array(sample))
+        i += 1
+        print(f"\b\b\b\b{i / len(test_data):.2f}", end="")
+    print(f"\rRF测试完成, 测试样本数: {len(test_data)}, 测试准确率: {t / len(test_data):.4f}")
+
+
+if __name__ == '__main__':
+    cli()
